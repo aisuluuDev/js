746b28b895b151e4d9426e36b9cf7f1801ba7094
aisuluuDev <aisuluuasanagazieva@gmail.com>
2023-02-21 18:11:18 +0600

    intitial

---
+++ script.js
@@ -0,0 +1,26 @@
+// Операторы сравнение
+
+// console.log(5 > 2);
+// console.log(3 <= 3);
+// console.log(3 != 4); //барабар эмес
+// console.log(3 == 3);
+// 
+// let num = 5== 5
+// console.log(num);
+// let num2 = '5 == 5'
+// console.log(num2);
+
+// console.log( true < 5); //true = 1, false = 0
+// console.log( false > true);
+
+// let a = '0'
+// console.log(typeof a);
+// console.log(a == 0);
+// 
+
+// console.log(5 != '5');
+// console.log(null == underfined);
+// console.log(null === underfined);
+// 
+// let year = prompt ('Азыр канчанчы жыл?')
+// console.log(typeof year);
